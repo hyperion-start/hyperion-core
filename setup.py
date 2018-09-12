b730e2af2702bdf2b3211d8b768989313e692e5f
dleins <dleins@techfak.uni-bielefeld.de>
2018-09-12 10:56:59 +0200

    Adds yaml files and setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+VERSION = '0.1'
+
+setup(
+    name='hyperion',
+    packages=find_packages(),
+    entry_points={
+    'console_scripts': [
+        'hyperion=hyperion:main',
+		    ],
+		},
+    version=VERSION,
+    install_requires=['webnsock', 'libtmux', 'psutil'],
+    description='The Hyperion Launch Engine',
+    author='David Leins',
+    author_email='dleins@techfak.uni-bielefeld.de',
+    url='https://github.com/DavidPL1/Hyperion.git',
+    keywords=['webnsock', 'libtmux'],
+    classifiers=[],
+    include_package_data=True,
+    zip_safe=False
+)
