a91bad3556942fbaff7a0e061b8e108cbc9fc293
dleins <dleins@techfak.uni-bielefeld.de>
2018-09-22 01:03:14 +0200

    Remove hold param for 'show term' xterms
    This keeps xterms from staying opened after the attached tmux session is
    killed.

--- hyperion/scripts/show_term.sh
+++ hyperion/scripts/show_term.sh
@@ -14,6 +14,6 @@ script_dir="$(dirname "${BASH_SOURCE[0]}")/show_session.sh $1"
 echo $script_dir
 
 echo "Launching xterm to attach session"
-xterm -hold -e "$script_dir $args"
+xterm -e "$script_dir $args"
 
 exit 0
