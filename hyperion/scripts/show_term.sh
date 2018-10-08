8f199bee45634186168ee31d9fb74b86f315434d
dleins <dleins@techfak.uni-bielefeld.de>
2018-10-08 23:15:02 +0200

    Show term script handles remote and local calls

--- hyperion/scripts/show_term.sh
+++ hyperion/scripts/show_term.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]
+if [ $# -lt 1 ]
   then
     # Incorrect amount of arguments supplied
-    echo "-1"
+    echo "$@"
     exit 1
 fi
 
 args=("$@")
-echo $args
 
 script_dir="$(dirname "${BASH_SOURCE[0]}")/show_session.sh $1"
 echo $script_dir
 
-echo "Launching xterm to attach session"
-xterm -e "$script_dir $args"
+comp_name=${args[0]}
+
+if [ $# -eq 2 ]
+  then
+    host=${args[1]}
+    echo "Launching xterm to attach remote session"
+    xterm -e "ssh -t $host 'bash -c \"tmux attach-session -t $comp_name\"'"
+else
+    echo "Launching xterm to attach local session"
+    xterm -e "$script_dir $comp_name"
+fi
 
 exit 0
