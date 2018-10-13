96fef59e5cb95961534832c36c8cd6f3903e3974
dleins <dleins@techfak.uni-bielefeld.de>
2018-10-13 17:27:42 +0200

    Add browsing through groups and action popup
    While browsing through all components is possible now, no functionality
    regarding starting, stopping and checking have been implemented.

--- hyperion/user_interfaces/interactiveCLI.py
+++ hyperion/user_interfaces/interactiveCLI.py
@@ -1,109 +1,154 @@
 import urwid
 
-class ActionButton(urwid.Button):
-    def __init__(self, caption, callback):
-        super(ActionButton, self).__init__("")
-        urwid.connect_signal(self, 'click', callback)
-        self._w = urwid.AttrMap(urwid.SelectableIcon(caption, 1),
-            None, focus_map='reversed')
-
-class Place(urwid.WidgetWrap):
-    def __init__(self, name, choices):
-        super(Place, self).__init__(
-            ActionButton([u" > go to ", name], self.enter_place))
-        self.heading = urwid.Text([u"\nLocation: ", name, "\n"])
-        self.choices = choices
-        # create links back to ourself
-        for child in choices:
-            getattr(child, 'choices', []).insert(0, self)
-
-    def enter_place(self, button):
-        game.update_place(self)
-
-class Thing(urwid.WidgetWrap):
-    def __init__(self, name):
-        super(Thing, self).__init__(
-            ActionButton([u" * take ", name], self.take_thing))
-        self.name = name
-
-    def take_thing(self, button):
-        self._w = urwid.Text(u" - %s (taken)" % self.name)
-        game.take_thing(self)
 
 def exit_program(button):
     raise urwid.ExitMainLoop()
 
-map_top = Place(u'porch', [
-    Place(u'kitchen', [
-        Place(u'refrigerator', []),
-        Place(u'cupboard', [
-            Thing(u'jug'),
-        ]),
-    ]),
-    Place(u'garden', [
-        Place(u'tree', [
-            Thing(u'lemon'),
-            Thing(u'bird'),
-        ]),
-    ]),
-    Place(u'street', [
-        Place(u'store', [
-            Thing(u'sugar'),
-        ]),
-        Place(u'lake', [
-            Place(u'beach', []),
-        ]),
-    ]),
-])
 
-class ComponentMenu(object):
+class StateController(object):
     def __init__(self, cc):
 
         self.cc = cc
+        self.selected_group = 0
+        self.groups = []
 
-        body = [urwid.Text('Groups'), urwid.Divider()]
-        for g in cc.config['groups']:
-            button = urwid.Button(g['name'])
-            urwid.connect_signal(button, 'click', self.group_chosen, g)
-            body.append(urwid.AttrMap(button, None, focus_map='reversed'))
-        group_box = urwid.ListBox(body)
+        header_text = urwid.Text(u'%s' % cc.config['name'])
+        header = urwid.AttrMap(header_text, 'titlebar')
+        menu = urwid.Text([
+            u'Press (', ('refresh button', u'N'), u') for next or (', ('refresh button', u'P'), u') for previous group | ',
+            u'Press (', ('quit button', u'Q'), u') to quit.'
+        ])
 
-        self.active_comps = urwid.SimpleFocusListWalker([urwid.Text("Such empty")])
+        for g in self.cc.config['groups']:
+            self.groups.append(g)
+        self.groups.append({'name': 'All'})
 
-        self.title = urwid.Text(('reversed', u'Title'), align='center')
-        self.MainBox = urwid.Columns([group_box, urwid.ListBox(self.active_comps)])
+        groups = self.get_group_items()
 
+        group_bar = urwid.Columns(groups)
+        group_filler = self.group_filler = urwid.Filler(group_bar, valign='top', top=1)
 
-    def group_chosen(self, button, group):
-        self.active_comps[0] = (urwid.Pile([urwid.Text("%s@%s - Status: %s " % (t['name'], t['host'], "DUMMY")) for t
-                                            in group['components']]))
+        active_comps = self.active_comps = urwid.SimpleFocusListWalker([urwid.Text("Nothing to display")])
 
+        self.group_changed()
 
-class AdventureGame(object):
-    def __init__(self):
-        self.log = urwid.SimpleFocusListWalker([])
-        self.top = urwid.ListBox(self.log)
-        self.inventory = set()
-        self.update_place(map_top)
+        comp_box = urwid.ListBox(active_comps)
+        v_padding = urwid.Padding(comp_box, left=1, right=1)
+        state_box = urwid.LineBox(v_padding)
+        v_padding_states = urwid.Padding(state_box, left=2, right=2)
 
-    def update_place(self, place):
-        if self.log: # disable interaction with previous place
-            self.log[-1] = urwid.WidgetDisable(self.log[-1])
-        self.log.append(urwid.Pile([place.heading] + place.choices))
-        self.top.focus_position = len(self.log) - 1
-        self.place = place
+        main_body = self.main_body = urwid.Pile([group_filler, v_padding_states])
+        self.layout = urwid.Frame(header=header, body=main_body, footer=menu)
 
-    def take_thing(self, thing):
-        self.inventory.add(thing.name)
-        if self.inventory >= set([u'sugar', u'lemon', u'jug']):
-            response = urwid.Text(u'You can make lemonade!\n')
-            done = ActionButton(u' - Joy', exit_program)
-            self.log[:] = [response, done]
+    def refresh_groups(self):
+        self.group_filler.body = urwid.Columns(self.get_group_items())
+
+    def get_group_items(self):
+        groups = []
+        for g in self.cc.config['groups']:
+            grp = urwid.LineBox(urwid.Text(g['name']))
+            if g['name'] == self.groups[self.selected_group]['name']:
+                grp = urwid.AttrMap(grp, 'group_selected')
+            else:
+                grp = urwid.AttrMap(grp, 'group')
+            groups.append(grp)
+
+        all_group = urwid.LineBox(urwid.Text(u'All'))
+        if self.groups[self.selected_group]['name'] == 'All':
+            all_group = urwid.AttrMap(all_group, 'group_selected')
         else:
-            self.update_place(self.place)
+            all_group = urwid.AttrMap(all_group, 'group')
+        groups.append(all_group)
+
+        return groups
+
+    def change_group(self, val):
+        if len(self.groups) > self.selected_group + val > -1:
+            self.selected_group = self.selected_group + val
+            self.group_changed()
+
+    def comp_action_cb(self, button, action):
+        main_loop.widget = self.layout
+
+    def selected_comp(self, comp_button, comp):
+        body = [urwid.Text("%s component menu" % comp['name']), urwid.Divider()]
+
+        choices = u'Start Stop Check Log'.split()
+        for c in choices:
+            button = urwid.Button(c)
+            urwid.connect_signal(button, 'click', self.comp_action_cb, c)
+            body.append(urwid.AttrMap(button, None, focus_map='reversed'))
+
+        box = urwid.ListBox(urwid.SimpleFocusListWalker(body))
+        lb = urwid.AttrMap(urwid.LineBox(box), 'popup')
+
+        overlay = urwid.Overlay(lb, self.main_body, align='center', width=('relative', 60),
+                      valign='middle', height=('relative', 60))
+        main_loop.widget = urwid.Frame(overlay)
+
+    def group_changed(self):
+        group = self.groups[self.selected_group]
+
+        comps = []
+        if group['name'] != 'All':
+            for c in group['components']:
+                button = urwid.Button("%s@%s - Status: %s " % (c['name'], c['host'], "DUMMY"))
+                urwid.connect_signal(button, 'click', self.selected_comp, c)
+                comps.append(urwid.AttrMap(button, None, focus_map='reversed'))
+        else:
+            for grp in self.groups:
+                if grp['name'] != 'All':
+                    for c in grp['components']:
+                        button = urwid.Button("%s@%s - Status: %s " % (c['name'], c['host'], "DUMMY"))
+                        urwid.connect_signal(button, 'click', self.selected_comp, c)
+                        comps.append(urwid.AttrMap(button, None, focus_map='reversed'))
+
+        self.active_comps[0] = urwid.Pile(comps)
+        self.refresh_groups()
+
+    def handle_input(self, key):
+        if key == 'R' or key == 'r':
+            refresh(main_loop, '')
+
+        if key == 'Q' or key == 'q':
+            raise urwid.ExitMainLoop()
+
+        if key == 'p' or key == 'P':
+            self.change_group(-1)
+        if key == 'n' or key == 'N':
+            self.change_group(1)
+
+        if key == 'esc':
+            main_loop.widget = self.layout
+
 
 def main(cc):
-    menu = ComponentMenu(cc)
-    urwid.MainLoop(menu.MainBox, palette=[('reversed', 'standout', '')]).run()
+    cli_menu = StateController(cc)
 
-game = AdventureGame()
+    palette = [
+        ('titlebar', 'dark red', ''),
+        ('popup', 'black', 'dark cyan'),
+        ('refresh button', 'dark green,bold', ''),
+        ('reversed', 'standout', ''),
+        ('quit button', 'dark red', ''),
+        ('button', 'black', 'light gray'),
+        ('button_select', 'light gray', 'black'),
+        ('group', 'black', "white"),
+        ('group_selected', 'white', 'dark blue'),
+        ('getting quote', 'dark blue', ''),
+        ('headers', 'white,bold', ''),
+        ('change ', 'dark green', ''),
+        ('change negative', 'dark red', '')]
+
+    global main_loop
+    main_loop = urwid.MainLoop(cli_menu.layout, palette, unhandled_input=cli_menu.handle_input, pop_ups=True)
+    # main_loop.set_alarm_in(0, refresh)
+    main_loop.run()
+
+
+def refresh(_loop, _data):
+    main_loop.draw_screen()
+    main_loop.set_alarm_in(10, refresh)
+
+
+main_loop = None
