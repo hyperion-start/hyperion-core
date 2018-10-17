934a930c3cd75b83a148d93ea2ede715e0d5a727
dleins <dleins@techfak.uni-bielefeld.de>
2018-10-17 18:18:59 +0200

    Split process monitoring in more classes
    The process monitoring class has been split into a local and a remote
    monitoring class. The same happened to the crash event class.
    
    Functionality for a remote host check has been implemented: a ssh
    connection is opened and a ps specifically requesting the remote pid is
    executed. This check is only executed though, if the remote host is
    reachable.
    
    The main monitoring thread now sorts the jobs in its queue to first
    process host monitoring and then component monitoring. This prevents
    firing a lot of crash events, if the connection of a host is lost. The
    processes of a host that the connection was lost to are simply set back
    into the queue to process as soon as the connection is restored.
    
    Also docstring was added for threads.py
    
    Refs: #16, #17

--- hyperion/lib/monitoring/threads.py
+++ hyperion/lib/monitoring/threads.py
@@ -1,42 +1,140 @@
-import threading
+from threading import Thread, Lock
 import logging
 import sys
 import time
+import hyperion.lib.util.config as config
+from subprocess import call
 from psutil import Process, NoSuchProcess
 is_py2 = sys.version[0] == '2'
 if is_py2:
     import Queue as Queue
 else:
     import queue as Queue
-
+    
 
 class ComponentMonitorJob(object):
-    def __init__(self, pid, comp_name, host=None):
+    """Abstract class that represents a component monitoring job (local or remote)."""
+
+    def __init__(self, pid, comp_name):
+        """Initializes component monitoring job.
+
+        :param pid: Process id of the component
+        :type pid: int
+        :param comp_name: Name of the component
+        :type comp_name: str
+        """
         self.pid = pid
         self.comp_name = comp_name
-        self.host = host
 
     def run_check(self):
-        if self.host:
-            print("No remote monitoring implemented yet")
-            return True
-        else:
-            try:
-                proc = Process(self.pid)
-                if proc.is_running():
-                    return True
-            except NoSuchProcess:
-                pass
+        """You need to override this function in monitoring subclasses. It is called in the main monitoring thread.
+
+        :return: True on a successful check, otherwise a CrashEvent is generated
+        :rtype: bool or CrashEvent
+        """
+
+
+class LocalComponentMonitoringJob(ComponentMonitorJob):
+    """Class that represents a local component monitoring job."""
+
+    def __init__(self, pid, comp_name):
+        """Creates a monitoring job for a local component.
+
+        :param pid: Process id of the component
+        :type pid: int
+        :param comp_name: Name of the component
+        :type comp_name: str
+        """
+
+        super(LocalComponentMonitoringJob, self).__init__(pid, comp_name)
+
+    def run_check(self):
+        """Runs a check if the pid exists and has not finished yet.
+
+        :return: True if the component is running, otherwise returns a generated ``LocalCrashEvent``
+        :rtype bool or LocalCrashEvent
+        """
+        try:
+            proc = Process(self.pid)
+            if proc.is_running():
+                return True
+        except NoSuchProcess:
+            pass
         return CrashEvent(self.comp_name)
 
     def info(self):
-        return "Running check for component %s with pid %s" % (self.comp_name, self.pid)
+        """Generate a status information for the job describing what is being monitored.
+
+        :return: Information about this job
+        :rtype: str
+        """
+
+        return "Running check for local component %s with pid %s" % (self.comp_name, self.pid)
+
+
+class RemoteComponentMonitoringJob(ComponentMonitorJob):
+    """Class that represents a remote component monitoring job."""
+
+    def __init__(self, pid, comp_name, hostname, host_status):
+        """Creates a remote component monitoring job.
+
+        :param pid: Process id on the remote machine
+        :type pid: int
+        :param comp_name: Name of the monitored component
+        :type comp_name: str
+        :param hostname: Name of the host running the component
+        :type hostname: str
+        """
+
+        super(RemoteComponentMonitoringJob, self).__init__(pid, comp_name)
+        self.hostname = hostname
+        self.host_status = host_status
+
+    def run_check(self):
+        """Runs a check if a remote process is still running.
+
+        :return: True if the component is still running or the host is not reachable, otherwise a ``RemoteCrashEvent`` is generated.
+        :rtype: bool or RemoteCrashEvent
+        """
+
+        if self.host_status.get(self.hostname):
+            cmd = 'ssh -F %s %s "ps -p %s > /dev/null"' % (config.CUSTOM_SSH_CONFIG_PATH, self.hostname, self.pid)
+            if call(cmd, shell=True) == 0:
+                return True
+            else:
+                return RemoteCrashEvent(self.comp_name, self.hostname)
+        # Return true because no information can be retrieved. The connection to the host has to be reestablished first.
+        return True
+
+    def info(self):
+        """Generate a status information for the job describing what is being monitored.
+
+        :return: Information about this job
+        :rtype: str
+        """
+
+        return "Running check for remote component %s with pid %s on host %s" % (self.comp_name, self.pid,
+                                                                                 self.hostname)
 
 
 class HostMonitorJob(object):
-    def __init__(self, pid, hostname):
+    """Class representing a host monitoring job."""
+    def __init__(self, pid, hostname, host_status, host_lock):
+        """Create host monitoring job.
+
+        :param pid: Process id of the ssh connection
+        :type pid: int
+        :param hostname: Name of the host connected to
+        :type hostname: str
+        :param host_status: Status of the used hosts
+        :type host_status: dict
+        :param host_lock: Lock that has to be acquired in order to write to the host status dictionary.
+        :type host_lock: Lock
+        """
         self.pid = pid
         self.hostname = hostname
+        self.host_status = host_status
+        self.host_lock = host_lock
 
     def run_check(self):
         try:
@@ -45,6 +143,11 @@ class HostMonitorJob(object):
                 return True
         except NoSuchProcess:
             pass
+
+        self.host_lock.acquire()
+        self.host_status[self.hostname] = None
+        self.host_lock.release()
+
         return DisconnectEvent(self.hostname)
 
     def info(self):
@@ -52,17 +155,76 @@ class HostMonitorJob(object):
 
 
 class CrashEvent(object):
+    """Superclass to model a component crash.
+
+    Provides the name of the crashed component."""
+
     def __init__(self, comp_name):
+        """Initializes the crash event assigning the component name
+
+        :param comp_name: Name of the crashed component
+        :type comp_name: str
+        """
+
         self.comp_name = comp_name
 
 
-class DisconnectEvent(object):
-    def __init__(self, hostname):
+class LocalCrashEvent(CrashEvent):
+    """Crash event subclass for local component crashes.
+
+    Provides the name of the crashed component and a short message.
+    """
+
+    def __init__(self, comp_name):
+        """Creates a local crash event class with a component name and generates a short message.
+
+        :param comp_name: Name of the crashed component
+        :type comp_name: str
+        """
+
+        super(LocalCrashEvent, self).__init__(comp_name)
+        self.message = 'Component %s crashed on localhost' % comp_name
+
+
+class RemoteCrashEvent(CrashEvent):
+    """Crash event subclass for remote component crashes.
+
+    Provides the name of the crashed component along with the host it ran on and a short message.
+    """
+
+    def __init__(self, comp_name, hostname):
+        """Creates a remote crash event with a component name and a host generating a short message.
+
+        :param comp_name: Name of the crashed component
+        :type comp_name: str
+        :param hostname: Name of the host the component was running on
+        :type hostname: str
+        """
+
+        super(RemoteCrashEvent, self).__init__(comp_name)
         self.hostname = hostname
+        self.message = 'Component %s crashed on remote host %s' % (comp_name, hostname)
 
 
-class MonitoringThread(threading.Thread):
+class DisconnectEvent(object):
+    """Class representing a disconnect event for remote hosts."""
+
+    def __init__(self, hostname):
+        """Creates a disconnect event with a hostname and generates a short message."""
+        self.hostname = hostname
+        self.message = 'Lost connection to remote host %s' % hostname
+
+
+class MonitoringThread(Thread):
+    """This class is monitoring thread that extends the threading.Thread class."""
+
     def __init__(self, queue):
+        """Initializes the monitoring thread with its input queue.
+
+        :param queue: Input queue the monitor retrieves its jobs from
+        :type queue: Queue.Queue
+        """
+
         logger = logging.getLogger(__name__)
         logger.setLevel(logging.DEBUG)
         logger.debug("Initialized thread")
@@ -72,27 +234,50 @@ class MonitoringThread(threading.Thread):
         self.end = False
 
     def kill(self):
+        """Shuts down the thread by signalling the run function to end.
+
+        :return: None
+        """
+
         logger = logging.getLogger(__name__)
         logger.debug("Killing process monitoring thread")
         self.end = True
 
     def add_subscriber(self, queue):
+        """Adds a subscriber to the list of queues to send notifications to.
+
+        :param queue: Subscribing queue that will get notifications by this thread
+        :type queue: Queue.Queue
+        :return: None
+        """
+
         logger = logging.getLogger(__name__)
         logger.debug("Added subscriber")
         self.subscribed_queues.append(queue)
 
     def run(self):
+        """Starts the monitoring thread.
+
+        :return: None
+        """
+
         logger = logging.getLogger(__name__)
         logger.setLevel(logging.DEBUG)
         logger.debug("Started run funtion")
         while not self.end:
 
+            comp_jobs = []
             jobs = []
             # Get all enqueued jobs for this iteration
             while not self.job_queue.empty():
                 mon_job = self.job_queue.get()
-                logger.debug("Adding job %s to list" % mon_job.pid)
-                jobs.append(mon_job)
+                if isinstance(mon_job, HostMonitorJob):
+                    jobs.append(mon_job)
+                if isinstance(mon_job, ComponentMonitorJob):
+                    comp_jobs.append(mon_job)
+
+            # Reorder job list to first check the hosts, then check the components because this makes sense
+            jobs.extend(comp_jobs)
 
             for mon_job in jobs:
                 logger.debug(mon_job.info())
