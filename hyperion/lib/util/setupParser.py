9788b5dc7420e7c18d0c0048c2a691911ee73a2a
dleins <dleins@techfak.uni-bielefeld.de>
2018-10-19 19:49:25 +0200

    Add docstring for setupParser.py

--- hyperion/lib/util/setupParser.py
+++ hyperion/lib/util/setupParser.py
@@ -1,21 +1,20 @@
 import yaml
 import os.path
 
+
 # This is a solution provided by Josh Bode in stackoverflow to provide import
 # https://stackoverflow.com/questions/528281/how-can-i-include-an-yaml-file-inside-another
 class Loader(yaml.SafeLoader):
+    """Custom loader class to feature including yaml files inside each other."""
 
     def __init__(self, stream):
-
         self._root = os.path.split(stream.name)[0]
-
         super(Loader, self).__init__(stream)
 
     def include(self, node):
-
         filename = os.path.join(self._root, self.construct_scalar(node))
-
         with open(filename, 'r') as f:
             return yaml.load(f, Loader)
+
 
 Loader.add_constructor('!include', Loader.include)
