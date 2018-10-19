3004fc83c1a01c1b92a3bf1dfeff69790c552387
dleins <dleins@techfak.uni-bielefeld.de>
2018-10-19 19:39:17 +0200

    Add docstring for depTree.py
    Refs: #17

--- hyperion/lib/util/depTree.py
+++ hyperion/lib/util/depTree.py
@@ -2,18 +2,44 @@ from exception import CircularReferenceException
 
 
 class Node(object):
+    """This class models a component as node of a dependency tree."""
     pass
 
     def __init__(self, comp):
+        """Initialize node for component ``comp``.
+
+        :param comp: Component to create a node for
+        :type comp: dict
+        """
+
         self.component = comp
         self.depends_on = []
         self.comp_name = comp['name']
 
     def add_edge(self, node):
+        """Add a node as dependency.
+
+        :param node: Node to add as dependency
+        :type node: Node
+        :return: None
+        """
+
         self.depends_on.append(node)
 
 
 def dep_resolve(node, resolved, unresolved):
+    """Recursively generate a list of all dependencies for ``node``
+
+    :param node: Node to resolve dependencies for
+    :type node: Node
+    :param resolved: List of already resolved nodes
+    :type resolved: List of Node
+    :param unresolved: List of unresolved nodes
+    :type unresolved: List of Node
+    :return: List containing all dependencies of ``node``
+    :rtype: List of Node
+    """
+
     unresolved.append(node)
     for edge in node.depends_on:
         if edge not in resolved:
