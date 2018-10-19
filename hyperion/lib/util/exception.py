d570c6066c869385a7372be44c3a4d349bde5c16
dleins <dleins@techfak.uni-bielefeld.de>
2018-10-19 19:45:37 +0200

    Add docstring for exception.py

--- hyperion/lib/util/exception.py
+++ hyperion/lib/util/exception.py
@@ -1,10 +1,26 @@
 
 class WindowNotFoundException(Exception):
+    """Exception to be thrown when a tmux window could not be found on the server."""
     def __init__(self, message):
+        """Create WindowNotFoundException with message ``message``.
+
+        :param message: Message to provide
+        :type message: str
+        """
+        
         super(Exception, self).__init__(message)
 
 
 class CircularReferenceException(Exception):
+    """Exception to be thrown when a circular dependency is detected in the dependency graph."""
     def __init__(self, node1, node2):
+        """Create CircularReferenceException between ``node1`` and ``node2.``
+
+        :param node1: First node involved
+        :type node1: Node
+        :param node2: Second node involved
+        :type node2: Node
+        """
+
         self.node1 = node1
         self.node2 = node2
