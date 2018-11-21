201d830b8d183972ffaebd5908355511dae27232
dleins <dleins@techfak.uni-bielefeld.de>
2018-11-21 22:28:00 +0100

    Adds basic server and remoteClient classes
    Server wraps around the hyperion core and remoteClient wraps around a
    ui. Both act as an interface for communication and use the
    actionSerializer and sockets for communication.
    The philosophy of the remoteClient is to keep calls of a ui the same as
    if it would run in standalone mode, meaning each callable function or
        accessible member of the hyperion core needs to have a counterpart
        in the remoteClient class (either resulting in a message to the
        server or returning information gained by previous communication
        with the server).
    
    Currently only config and host_list are received by the client, but the
    basic communication works and is easily extendable.
    
    Refs: #23

---
+++ hyperion/lib/networking/clientInterface.py
@@ -0,0 +1,181 @@
+import selectors2 as selectors
+import socket
+import time
+import logging
+import sys
+import struct
+import threading
+import hyperion.lib.util.actionSerializer as actionSerializer
+from hyperion.manager import AbstractController
+
+is_py2 = sys.version[0] == '2'
+if is_py2:
+    import Queue as queue
+else:
+    import queue as queue
+
+
+def recvall(connection, n):
+    """Helper function to recv n bytes or return None if EOF is hit
+    
+    To read a message with an expected size and combine it to one object, even if it was split into more than one 
+    packets.
+    
+    :param connection: Connection to a socket
+    :param n: Size of the message to read in bytes
+    :type n: int
+    :return: Expected message combined into one string
+    """
+
+    data = b''
+    while len(data) < n:
+        packet = connection.recv(n - len(data))
+        if not packet:
+            return None
+        data += packet
+    return data
+
+
+class RemoteControllerInterface(AbstractController):
+    def __init__(self, host, port):
+        super(RemoteControllerInterface, self).__init__(None)
+        self.host_list = None
+        self.config = None
+        self.host = host
+        self.port = port
+        self.logger = logging.getLogger(__name__)
+        self.receive_queue = queue.Queue()
+        self.send_queue = queue.Queue()
+        self.mysel = selectors.DefaultSelector()
+        self.keep_running = True
+
+        self.function_mapping = {
+            'get_config_response': self._set_config,
+            'get_host_list_response': self._set_host_list
+        }
+
+        server_address = (host, port)
+        self.logger.debug('connecting to {} port {}'.format(*server_address))
+        self.sock = sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            sock.connect(server_address)
+        except socket.error:
+            self.logger.error("Master session does not seem to be running. Quitting remote client")
+            self.cleanup()
+        sock.setblocking(False)
+
+        # Set up the selector to watch for when the socket is ready
+        # to send data as well as when there is data to read.
+        self.mysel.register(
+            sock,
+            selectors.EVENT_READ | selectors.EVENT_WRITE,
+        )
+
+        self.thread = threading.Thread(target=self.loop)
+        self.thread.start()
+
+        self.request_config()
+        while not self.config or not self.host_list:
+            self.logger.debug("Waiting for config")
+            time.sleep(0.5)
+
+    def request_config(self):
+        action = 'get_conf'
+        payload = []
+        message = actionSerializer.serialize_request(action, payload)
+        self.send_queue.put(message)
+
+        action = 'get_host_list'
+        message = actionSerializer.serialize_request(action, payload)
+        self.send_queue.put(message)
+
+    def cleanup(self):
+        # TODO: signalize to client endpoint (UI) to shut down.
+        sys.exit(1)
+
+    def get_component_by_name(self, comp_name):
+        self.logger.debug("Serializing component fetch")
+
+        action = 'get_comp'
+        payload = [comp_name]
+
+        message = actionSerializer.serialize_request(action, payload)
+        self.send_queue.put(message)
+
+    def get_dep_list(self, comp):
+        self.logger.debug("Serializing component dep list")
+        action = 'dep_list'
+        payload = [comp]
+
+        message = actionSerializer.serialize_request(action, payload)
+        self.send_queue.put(message)
+
+    def kill_session_by_name(self, session_name):
+        self.logger.debug("Serializing kill session by name")
+        action = 'kill_session'
+        payload = [session_name]
+
+        message = actionSerializer.serialize_request(action, payload)
+        self.send_queue.put(message)
+
+    def start_component_without_deps(self, comp):
+        self.logger.debug("Serializing component start")
+        action = 'start'
+        payload = [comp]
+
+        message = actionSerializer.serialize_request(action, payload)
+        self.send_queue.put(message)
+
+    def stop_component(self, comp):
+        self.logger.debug("Serializing component stop")
+        action = 'stop'
+        payload = [comp]
+
+        message = actionSerializer.serialize_request(action, payload)
+        self.send_queue.put(message)
+
+    def run_component_check(self, comp):
+        self.logger.debug("Serializing component check")
+        action = 'check'
+        payload = [comp]
+
+        message = actionSerializer.serialize_request(action, payload)
+        self.send_queue.put(message)
+
+    def _interpret_message(self, action, args):
+        func = self.function_mapping.get(action)
+        func(*args)
+
+    def _set_config(self, config):
+        self.config = config
+        self.logger.debug("Got config from server")
+
+    def _set_host_list(self, host_list):
+        self.host_list = host_list
+        self.logger.debug("Updated host list")
+
+    def loop(self):
+        while self.keep_running:
+            for key, mask in self.mysel.select(timeout=1):
+                connection = key.fileobj
+
+                if mask & selectors.EVENT_READ:
+                    self.logger.debug("Got read event")
+                    raw_msglen = connection.recv(4)
+                    if raw_msglen:
+                        # A readable client socket has data
+                        msglen = struct.unpack('>I', raw_msglen)[0]
+                        data = recvall(connection, msglen)
+                        self.logger.debug("Received message")
+                        action, args = actionSerializer.deserialize(data)
+                        self._interpret_message(action, args)
+
+                    # Interpret empty result as closed connection
+                    else:
+                        self.keep_running = False
+
+                if mask & selectors.EVENT_WRITE:
+                    if not self.send_queue.empty():  # Server is ready to read, check if we have messages to send
+                        self.logger.debug("Sending next message in queue to Server")
+                        next_msg = self.send_queue.get()
+                        self.sock.sendall(next_msg)
---
+++ hyperion/lib/networking/server.py
@@ -0,0 +1,142 @@
+import socket
+import time
+import logging
+import sys
+import struct
+import hyperion.manager
+import hyperion.lib.util.depTree
+import hyperion.lib.util.actionSerializer as actionSerializer
+import hyperion.lib.util.config as config
+
+is_py2 = sys.version[0] == '2'
+if is_py2:
+    import Queue as queue
+else:
+    import queue as queue
+
+try:
+    import selectors
+except ImportError:
+    import selectors2 as selectors
+
+
+def recvall(connection, n):
+    """Helper function to recv n bytes or return None if EOF is hit
+
+    To read a message with an expected size and combine it to one object, even if it was split into more than one
+    packets.
+
+    :param connection: Connection to a socket
+    :param n: Size of the message to read in bytes
+    :type n: int
+    :return: Expected message combined into one string
+    """
+
+    data = b''
+    while len(data) < n:
+        packet = connection.recv(n - len(data))
+        if not packet:
+            return None
+        data += packet
+    return data
+
+
+class Server:
+    def __init__(self, port, cc):
+        self.port = port
+        self.sel = selectors.DefaultSelector()
+        self.keep_running = True
+        self.cc = cc  # type: hyperion.ControlCenter
+        self.logger = logging.getLogger(__name__)
+        self.send_queues = {}
+
+        server_address = ('localhost', port)
+        self.logger.debug("Starting server on localhost:%s" % port)
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.setblocking(False)
+        server.bind(server_address)
+        server.listen(5)
+
+        self.function_mapping = {
+            'run': self.cc.start_component_without_deps,
+            'check': self.cc.check_component,
+            'stop': self.cc.stop_component,
+            'list_all': self.cc.get_start_all_list,
+            'deps': self.cc.get_dep_list,
+            'get_conf': self._send_config,
+            'get_host_list': self._send_host_list
+        }
+
+        self.sel.register(server, selectors.EVENT_READ, self.accept)
+
+        while self.keep_running:
+            for key, mask in self.sel.select(timeout=1):
+                connection = key.fileobj
+                if key.data:
+                    callback = key.data
+                    callback(connection, mask)
+
+                else:
+                    if mask & selectors.EVENT_READ:
+                        self.read(connection)
+                    if mask & selectors.EVENT_WRITE:
+                        self.write(connection)
+            time.sleep(0.3)
+
+        print('shutting down')
+        self.sel.close()
+
+    def write(self, connection):
+        """Callback for write events"""
+        send_queue = self.send_queues.get(connection)
+        if not send_queue.empty():
+            # Messages available
+            next_msg = send_queue.get()
+            connection.sendall(next_msg)
+            logging.debug("Sending message to %s" % connection)
+
+    def read(self, connection):
+        """Callback for read events"""
+        raw_msglen = connection.recv(4)
+        if raw_msglen:
+            # A readable client socket has data
+            msglen = struct.unpack('>I', raw_msglen)[0]
+            data = recvall(connection, msglen)
+            self.logger.debug("Received message")
+            action, args = actionSerializer.deserialize(data)
+            self.interpret_message(action, args, connection)
+        else:
+            # Interpret empty result as closed connection
+            print('  closing')
+            self.sel.unregister(connection)
+            connection.close()
+            # Tell the main loop to stop
+            self.keep_running = False
+
+    def accept(self, sock, mask):
+        "Callback for new connections"
+        new_connection, addr = sock.accept()
+        print('accept({})'.format(addr))
+        new_connection.setblocking(False)
+        self.send_queues[new_connection] = queue.Queue()
+        self.sel.register(new_connection, selectors.EVENT_READ | selectors.EVENT_WRITE)
+
+    def interpret_message(self, action, args, connection):
+        self.logger.debug("Action: %s, args: %s" % (action, args))
+        func = self.function_mapping.get(action)
+        self.logger.debug("Calling function %s" % func)
+        func(connection, *args)
+
+    def _send_config(self, connection):
+        action = 'get_config_response'
+        payload = [self.cc.config]
+
+        message = actionSerializer.serialize_request(action, payload)
+        self.send_queues[connection].put(message)
+
+    def _send_host_list(self, connection):
+        action = 'get_host_list_response'
+        payload = [self.cc.host_list]
+
+        message = actionSerializer.serialize_request(action, payload)
+        self.send_queues[connection].put(message)
